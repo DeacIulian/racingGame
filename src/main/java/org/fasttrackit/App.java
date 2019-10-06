3217753aa0931ac1148e94986ad8ae0be3068c74
Deac Iulian <deaciulian91@gmail.com>
2019-10-07 01:56:32 +0300

    Studied checked exceptions

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -8,7 +8,7 @@ import java.util.concurrent.Callable;
  */
 public class App 
 {
-    public static void main( String[] args ) {
+    public static void main( String[] args ) throws Exception {
         Game game = new Game();
         game.start();
 
--- src/main/java/org/fasttrackit/Game.java
+++ src/main/java/org/fasttrackit/Game.java
@@ -14,7 +14,7 @@ public class Game {
     private List<Vehicle> competitors = new ArrayList<>();
 
 
-    public void start() {
+    public void start() throws Exception {
         initializeTracks();
         displayTracks();
 
@@ -22,7 +22,7 @@ public class Game {
 
     }
 
-    private void initializeCompetitors() {
+    private void initializeCompetitors() throws Exception {
         int competitorCount = getCompetitorCountFromUser();
         System.out.println("Today's competitors are: ");
 
@@ -39,13 +39,13 @@ public class Game {
         }
     }
 
-    private int getCompetitorCountFromUser(){
+    private int getCompetitorCountFromUser() throws Exception {
         System.out.println("Please enter number of players: ");
         Scanner scanner = new Scanner(System.in);
         try {
             return scanner.nextInt();
         } catch (InputMismatchException e) {
-            throw new RuntimeException("You have entered an invalid number. ");
+            throw new Exception("You have entered an invalid number. ");
         } finally {
             System.out.println("Finnaly block is always executed. ");
         }
